b303705484588ae57fdc8f6696ff14f91f4f429d
Lev Kokotov <lev.kokotov@gmail.com>
2022-01-24 11:53:29 -0800

    migrations!

---
+++ migrations/20220124194619_metrics.sql
@@ -0,0 +1,14 @@
+-- Add migration script here
+CREATE TABLE public.metric_names (
+    id BIGSERIAL PRIMARY KEY,
+    name character varying UNIQUE
+);
+
+CREATE TABLE public.metrics (
+    id BIGSERIAL PRIMARY KEY,
+    metric_id bigint REFERENCES public.metric_names(id),
+    value double precision,
+    recorded_at timestamp without time zone
+);
+
+CREATE INDEX ON public.metrics USING btree(recorded_at, metric_id);
